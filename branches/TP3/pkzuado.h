a980e68322c96c78be4ee473c6949896e1734295
david.kurka <david.kurka@8218270c-4048-0410-9d58-cd40a97f162f>
2008-04-15 21:02:15 +0000

    vetor dobravel ta quase saindo! :)

--- branches/TP3/pkzuado.h
+++ branches/TP3/pkzuado.h
@@ -3,27 +3,27 @@
 
 /*sempre que o vetor que armazena as chaves primarias estiver cheio, 
   ele eh dobrado de tamanho (vetor dinamico dobravel)*/
-void realoca_memoria(ap_tipo_registro_pk * vetor_pk, int * limite);
+ap_tipo_registro_pk realoca_memoria(ap_tipo_registro_pk vetor_pk, int * limite);
 
 /* le todos os dados do arquivo PK.dat e os adiciona em um vetor, mantendo a ordem de registros, que é alfabética */
-void lerArquivoPK(FILE *arq_pk, ap_tipo_registro_pk * vetor, int * limite, int n_registros);
+void lerArquivoPK(FILE *arq_pk, ap_tipo_registro_pk vetor, int * limite, int n_registros);
 
 /*caso a lista em PK.dat nao estivesse criada, le os dados da base.dat 
   e cria os indices no arquivo.*/
-void inserePKBase(FILE *arqBase, ap_tipo_registro_pk * vetor, int * limite, int n_registros);
+void inserePKBase(FILE *arqBase, ap_tipo_registro_pk vetor, int * limite, int n_registros);
 
 /*pega o ultimo titulo lido e registra ele como novo,para ser inserido no vetor de chaves primarias*/
-int novopk(char *str_final, ap_tipo_registro_pk * vetor, int * limite);
+int novopk(char *str_final, ap_tipo_registro_pk vetor, int * limite);
 
 /* insere um novo registro no nosos vetor dinamico de PKs, essa inserção é ordenada e mantém a ordem alfabética do vetor */
-void insere_pk(ap_tipo_registro_pk * vetor_pk, tipo_registro_pk novo, int * limite);
+ap_tipo_registro_pk insere_pk(ap_tipo_registro_pk vetor_pk, tipo_registro_pk novo, int * limite);
 
 /* salva todos os registros do nosso vetor de PKs no arquivo PK.dat*/
-void salvarArquivoPK(ap_tipo_registro_pk * vetor, FILE *arq_pk, int limite_reg);
+void salvarArquivoPK(ap_tipo_registro_pk vetor, FILE *arq_pk, int limite_reg);
 
 
 /* Consulta por chave primaria */
-void consulta_pk(int limite_reg, ap_tipo_registro_pk  vetor_de_registros, FILE *arqBase);
+void consulta_pk(int limite_reg, ap_tipo_registro_pk vetor_de_registros, FILE *arqBase);
 
 /* Lista todas as chaves primarias presentes*/
 void lista_registros( int limite_reg,  ap_tipo_registro_pk vetor_de_registros);
