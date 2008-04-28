f7e9bc7f161c9544576e2b81f7fc81600cfd819a
vitorantonioli <vitorantonioli@8218270c-4048-0410-9d58-cd40a97f162f>
2008-04-28 22:57:20 +0000

    /* Definicao das funcoes de manipulacao    no arquivo de chaves secundarias. */
    /*cria vetores de SKs, um para cada chave secundaria, assim como as suas respectivas listas invertidas*/
    tipo_vetores_sk * criarVetorSK(FILE *arqBase, int n_registros);

--- trunk/TP3/sk.h
+++ trunk/TP3/sk.h
@@ -1,2 +1,6 @@
 /* Definicao das funcoes de manipulacao
    no arquivo de chaves secundarias. */
+
+/*cria vetores de SKs, um para cada chave secundaria, assim como as suas respectivas listas invertidas*/
+tipo_vetores_sk * criarVetorSK(FILE *arqBase, int n_registros);
+
