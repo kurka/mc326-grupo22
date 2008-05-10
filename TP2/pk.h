2e6912ef0835f379b4fed485814185c033c5c791
david.kurka <david.kurka@8218270c-4048-0410-9d58-cd40a97f162f>
2008-05-10 03:07:45 +0000

    versao final do TP2 This line, and those below, will be ignored--
    M    TP2/base.h
    M    TP2/pk.h
    M    TP2/defines.h
    M    TP2/main.c
    M    TP2/base.c
    M    TP2/pk.c
    M    TP2/Makefile

--- TP2/pk.h
+++ TP2/pk.h
@@ -1,29 +1,32 @@
 /* Funcoes de operacoes a serem realizadas nos arquivos 
    referentes a chaves primarias */
 
+/*sempre que o vetor que armazena as chaves primarias estiver cheio, 
+  ele eh dobrado de tamanho (vetor dinamico dobravel)*/
+ap_tipo_registro_pk realoca_memoria(ap_tipo_registro_pk vetor_pk, int * limite);
 
 /* le todos os dados do arquivo PK.dat e os adiciona em um vetor, mantendo a ordem de registros, que é alfabética */
-ap_tipo_registro_pk lerArquivoPK(FILE *arq_pk, int numberOfPKs);
+ap_tipo_registro_pk lerArquivoPK(FILE *arq_pk, ap_tipo_registro_pk vetor, int * limite, int n_registros);
 
-/*caso a lista em PK.dat nao estivesse criada, le os dados da base.dat 
+/* caso a lista em PK.dat nao estivesse criada, le os dados da base.dat 
   e cria os indices no arquivo.*/
-ap_tipo_registro_pk inserePKBase(FILE *arqBase, int numberOfPKs);
+ap_tipo_registro_pk inserePKBase(FILE *arqBase, ap_tipo_registro_pk vetor, int * limite, int n_registros);
 
 /*pega o ultimo titulo lido e registra ele como novo,para ser inserido no vetor de chaves primarias*/
-tipo_registro_pk novopk(char *str_final, int numberOfPKs);
+ap_tipo_registro_pk novopk(char *str_final, ap_tipo_registro_pk vetor, int * limite);
 
 /* insere um novo registro no nosos vetor dinamico de PKs, essa inserção é ordenada e mantém a ordem alfabética do vetor */
-int inserirPK(tipo_registro_pk *vetor, tipo_registro_pk novo, int numberOfPKs);
+ap_tipo_registro_pk insere_pk(ap_tipo_registro_pk vetor_pk, tipo_registro_pk novo, int * limite);
 
 /* salva todos os registros do nosso vetor de PKs no arquivo PK.dat*/
-void salvarArquivoPK(tipo_registro_pk *vetor, FILE *arq_pk, int numberOfPKs);
+void salvarArquivoPK(ap_tipo_registro_pk vetor, FILE *arq_pk, int limite_reg);
 
 
 /* Consulta por chave primaria */
-void consulta_pk(int n_registros, ap_tipo_registro_pk vetor_de_registros, FILE *arqBase);
+void consulta_pk(int limite_reg, ap_tipo_registro_pk vetor_de_registros, FILE *arqBase);
 
 /* Lista todas as chaves primarias presentes*/
-void lista_registros( int n_registros,  ap_tipo_registro_pk vetor_de_registros);
+void lista_registros( int limite_reg,  ap_tipo_registro_pk vetor_de_registros);
 
 
 
