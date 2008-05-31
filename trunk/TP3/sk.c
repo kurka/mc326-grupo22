853a86a7f171afef9ba223206177ccab8046595b
david.kurka <david.kurka@8218270c-4048-0410-9d58-cd40a97f162f>
2008-05-31 21:18:27 +0000

    guarda parcial de chaves secundarias em arquivo

--- trunk/TP3/sk.c
+++ trunk/TP3/sk.c
@@ -457,43 +457,36 @@ tipo_vetorzao * criarVetorSK(FILE *arqBase, int n_registros)
       }
   }
   
+
   /*  return vetores_sk;*/
+  salvaArquivosLi(vetores_li);
+  
   return vetorzao;
 }
 
 
 
-
-
-/* Estrutura do arquivo de chaves secundarias:
-   [SK][Numero de PKs associadas][Chaves associadas (multiplo de 200)]
-   Exemplo:
-   Registros: "a ultima ceia","a santa ceia"
-
-   a 2 a ultima ceia             a santa ceia               
-   ultima 1 a ultima ceia        
-   ceia 2 a ultima ceia          a santa ceia               
-   santa 1 a santa ceia          
+/**
+    \brief salva todos os registros da lista invertida de chaves secundarias nos arquivos *li.dat
 */
+void salvaArquivosLi(tipo_vetores_li * vetores_li)
+{
+
+  FILE * arq_tit_li;
+  int i;
+
+  arq_tit_li = fopen("titulos_li.dat", "w+");  
 
 
-/*
-  1 Ao iniciar o programa, verifica se existe um arquivo de chaves secundarias
-  1.1 Existe sk.dat
-  1.2 Nao existe sk.dat
-  1.2.1 Se existe base.dat => gera um sk.dat
- */
-
-
-/*
-  Busca por indice secundario (recebe uma string e procura no arquivo de SK's ) 
- */
-
-
-/*
-  Remocao de registro -> avail list dos indices secundarios
-  Insercao de registro -> insere novas chaves secundarias do novo registro
- */
+  
+  fseek(arq_tit_li,0,SEEK_SET);
+  
+  for(i=0; i<vetores_li->n_titulos; i++){
+    fprintf(arq_tit_li, "%s", vetores_li->vetor_li_titulo[i].chave); 
+    fprintf(arq_tit_li, "%08d", vetores_li->vetor_li_titulo[i].prox);     
+  }
+  fclose(arq_tit_li); 
+}
 
 
 /* /\** */
