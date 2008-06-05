ec73a565f77b79ca6be2b57d6d8d738735dce134
felipe.eltermann <felipe.eltermann@8218270c-4048-0410-9d58-cd40a97f162f>
2008-06-05 20:56:13 +0000

    fopen.c/h e main.c comentados

--- trunk/TP3/fopen.h
+++ trunk/TP3/fopen.h
@@ -1,16 +1,16 @@
-/*funcoes usadas na abertura de arquivos*/
+/* Funcoes usadas na abertura de arquivos */
 
-/*confere se existe e abre ou cria arquivo base22.dat*/
+/* Confere se existe e abre ou cria arquivo base22.dat */
 FILE * abre_base22(FILE *arq_base, int *n_registros);
 
-/*confere se existe e abre ou cria arquivo pk.dat*/
+/* Confere se existe e abre ou cria arquivo pk.dat */
 FILE * abre_pk(FILE *arq_pk, int *pk);
 
-/*confere se existe e abre ou cria arquivo cabeca_avail_base.dat*/
+/* Confere se existe e abre ou cria arquivo cabeca_avail_base.dat */
 FILE * abre_avail(FILE *arq_cabeca_avail_base, int *cabeca_avail_base);
 
-/*abre arquivos de listas invertidas (vinculados a chaves secundarias)*/
+/* Abre arquivos de listas invertidas (vinculados a chaves secundarias) */
 void abre_lis(tipo_arqs_li * arqs_li);
 
-/*para nao voltar abruptamente para o menu, pede para o usuario digitar algo para prosseguir*/
+/* Para nao voltar abruptamente para o menu, pede para o usuario digitar algo para prosseguir */
 void espera();
