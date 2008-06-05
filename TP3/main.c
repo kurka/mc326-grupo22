6b14c9c475b7528b406919d1660c2bf40afaf8ef
david.kurka <david.kurka@8218270c-4048-0410-9d58-cd40a97f162f>
2008-06-05 07:00:00 +0000

    concertada redundancias do codigo
    (todas as funcoes estao habilitadas novamente..)

--- TP3/main.c
+++ TP3/main.c
@@ -140,7 +140,7 @@ int main() {
       nrr = escreve_base(arq_base, arq_avail, str_final, &cabeca_avail);
       vetor_registros = novopk(str_final, vetor_registros, limite, nrr);   
       /*insere as novas sks*/
-/*       insereVetorSK(str_final, vetores_sk, arqs_li); */
+      vetores_sk = insereVetorSK(str_final, vetores_sk, arqs_li);
 
 
       if(DEBUG)     
@@ -157,19 +157,19 @@ int main() {
       consulta_pk(limite[0],vetor_registros, arq_base);
       break;
 
-/*       /\* Procurar por registros via chave secundaria *\/ */
-/*     case CONSULTA_SK_TIT: */
-/*       consulta_sk_tit(vetores_sk, vetor_registros, limite[0], arqs_li->arq_tit_li, arq_base); */
-/*       break; */
-/*     case CONSULTA_SK_TIP: */
-/*       consulta_sk_tip(vetores_sk, vetor_registros, limite[0], arqs_li->arq_tip_li, arq_base); */
-/*       break; */
-/*     case CONSULTA_SK_AUT: */
-/*       consulta_sk_aut(vetores_sk, vetor_registros, limite[0], arqs_li->arq_aut_li, arq_base); */
-/*       break; */
-/*     case CONSULTA_SK_ANO: */
-/*       consulta_sk_ano(vetores_sk, vetor_registros, limite[0], arqs_li->arq_ano_li, arq_base); */
-/*       break; */
+      /* Procurar por registros via chave secundaria */
+    case CONSULTA_SK_TIT:
+      consulta_sk_tit(vetores_sk, vetor_registros, limite[0], arqs_li->arq_tit_li, arq_base);
+      break;
+    case CONSULTA_SK_TIP:
+      consulta_sk_tip(vetores_sk, vetor_registros, limite[0], arqs_li->arq_tip_li, arq_base);
+      break;
+    case CONSULTA_SK_AUT:
+      consulta_sk_aut(vetores_sk, vetor_registros, limite[0], arqs_li->arq_aut_li, arq_base);
+      break;
+    case CONSULTA_SK_ANO:
+      consulta_sk_ano(vetores_sk, vetor_registros, limite[0], arqs_li->arq_ano_li, arq_base);
+      break;
 
       /* Remove um registro do catalogo */
     case REMOVER:
--- TP3/sk.h
+++ TP3/sk.h
@@ -11,15 +11,6 @@ tipo_vetores_sk * insereVetorSK(char *registro, tipo_vetores_sk *vetores_sk, tip
 /* cria vetor sk e lista invertida*/	
 tipo_dados_sk *cria_vetor_generico(char *registro, char *pk, tipo_dados_sk *generico, int *n_li_generica, FILE * arq_gen_li);
 
-/* /\* cria vetor sk e lista invertida para titulo *\/	 */
-/* void cria_vetor_titulo(char registro[TAM_REGISTRO], char pk[TAM_TIT+1], int *n_titulos_li, tipo_vetores_sk *vetores_sk, FILE *arq_tit_li); */
-/* cria vetor sk e lista invertida para tipo */	
-void cria_vetor_tipo(char registro[TAM_REGISTRO], char pk[TAM_TIT+1], int *n_tipos_li, tipo_vetores_sk *vetores_sk, FILE *arq_tip_li);
-/* cria vetor sk e lista invertida para autor */	
-void cria_vetor_autor(char registro[TAM_REGISTRO], char pk[TAM_TIT+1], int *n_autores_li, tipo_vetores_sk *vetores_sk, FILE *arq_aut_li);
-/* cria vetor sk e lista invertida para ano */	
-void cria_vetor_ano(char registro[TAM_REGISTRO], char pk[TAM_TIT+1], int *n_anos_li, tipo_vetores_sk *vetores_sk, FILE *arq_ano_li);
-
 
 tipo_registro_sk *realoca_memoria_sk(tipo_registro_sk *vetor_SK_generico, int *limite); 
 
@@ -28,6 +19,8 @@ void consulta_sk_tit(tipo_vetores_sk *vetores_sk, tipo_registro_pk *vetor_pk, in
 void consulta_sk_tip(tipo_vetores_sk *vetores_sk, tipo_registro_pk *vetor_pk, int n_pk, FILE *arq_tip_li, FILE *arq_base);
 void consulta_sk_aut(tipo_vetores_sk *vetores_sk, tipo_registro_pk *vetor_pk, int n_pk, FILE *arq_aut_li, FILE *arq_base);
 void consulta_sk_ano(tipo_vetores_sk *vetores_sk, tipo_registro_pk *vetor_pk, int n_pk, FILE *arq_ano_li, FILE *arq_base);
+
+void acha_sk(char *palavra_procurada, int n_pk, FILE *arq_base, FILE *arq_gen_li, tipo_dados_sk * generico, tipo_registro_pk *vetor_pk);
 
 /* Funcoes Auxiliares */
 
