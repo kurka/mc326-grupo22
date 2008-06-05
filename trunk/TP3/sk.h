6acbc580c5c5ea0221340d108fbbd613f6bbf641
felipe.eltermann <felipe.eltermann@8218270c-4048-0410-9d58-cd40a97f162f>
2008-06-05 22:51:14 +0000

    sk.c/h comentado... comecando documentacao em Doxygen

--- trunk/TP3/sk.h
+++ trunk/TP3/sk.h
@@ -1,35 +1,38 @@
-/* Definicao das funcoes de manipulacao
-   no arquivo de chaves secundarias. */
+/* Definicao das funcoes de manipulacao das chaves secundarias */
 
-/*cria vetores de SKs, um para cada chave secundaria, assim como as suas respectivas listas invertidas*/
+/* Cria vetores de SKs, um para cada chave secundaria, assim como as suas respectivas listas invertidas */
 tipo_vetores_sk * criarVetorSK(int n_registros, tipo_arqs_li * arqs_li, FILE *arqBase);
 
-/*insere um novo vetor de SKs, um para cada chave secundaria do novo registro, assim como as suas respectivas listas invertidas*/
+/* Insere um novo vetor de SKs, um para cada chave secundaria do novo registro, assim como as suas 
+   respectivas listas invertidas*/
 tipo_vetores_sk * insereVetorSK(char *registro, tipo_vetores_sk *vetores_sk, tipo_arqs_li * arqs_li );
 
 
-/* cria vetor sk e lista invertida*/	
+/* Cria vetor sk e lista invertida*/	
 tipo_dados_sk *cria_vetor_generico(char *registro, char *pk, tipo_dados_sk *generico, int *n_li_generica, FILE * arq_gen_li);
 
-
+/* Funcao de alocacao do vetor (dobravel) de SKs */
 tipo_registro_sk *realoca_memoria_sk(tipo_registro_sk *vetor_SK_generico, int *limite); 
 
-
+/* Estas funcoes "instanciam" os tipos das chaves secundarias (titulo, tipo, autor ou ano) */
 void consulta_sk_tit(tipo_dados_sk *titulo, tipo_registro_pk *vetor_pk, int n_pk, FILE *arq_tit_li, FILE *arq_base);
 void consulta_sk_tip(tipo_dados_sk *tipo, tipo_registro_pk *vetor_pk, int n_pk, FILE *arq_tip_li, FILE *arq_base);
 void consulta_sk_aut(tipo_dados_sk *autor, tipo_registro_pk *vetor_pk, int n_pk, FILE *arq_aut_li, FILE *arq_base);
 void consulta_sk_ano(tipo_dados_sk *ano, tipo_registro_pk *vetor_pk, int n_pk, FILE *arq_ano_li, FILE *arq_base);
 
+/* Procura por uma chave no vetor de chaves secundarias */
 void acha_sk(char *palavra_procurada, int n_pk, FILE *arq_base, FILE *arq_gen_li, tipo_dados_sk * generico, tipo_registro_pk *vetor_pk);
 
-/*funcao que le da entrada padrao e trata excecoes de entrada de busca*/
+/* Funcao que le da entrada padrao e trata excecoes de entrada de busca */
 int le_sk(char* palavra_procurada, int max);
+
+
 
 /* Funcoes Auxiliares */
 
-/*funcao analoga a strcmp, mas insensivel a maiusculas/minusculas */
+/* Funcao analoga a strcmp, mas insensivel a maiusculas/minusculas */
 int strcmpinsensitive(char * a, char * b);
-/* funcao auxiliar usada na funcao qsort (para ordenar sks)*/
+/* Funcao auxiliar usada na funcao qsort (para ordenar sks) */
 int compara_qsort2(const void * vetora, const void * vetorb);
-/*brief funcao auxiliar usada na funcao bsearch (para encontrar sks)*/
+/* Funcao auxiliar usada na funcao bsearch (para encontrar sks) */
 int compara_bsearch2(const void * titulo_procurado, const void * vetor_de_registros);
