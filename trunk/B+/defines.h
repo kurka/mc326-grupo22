ecb24df4308bcc40de7210f7c3bbee63c3122f4e
felipe.eltermann <felipe.eltermann@8218270c-4048-0410-9d58-cd40a97f162f>
2008-06-21 03:21:51 +0000

    Adicionado main.c (em branco), defines.h (quase em branco) e Makefile...

---
+++ trunk/B+/defines.h
@@ -0,0 +1,18 @@
+
+/* Constantes numericas */
+#define OK 0
+#define ERRO -1
+#define ERRO2 -2
+#define DEBUG 0/*if(DEBUG) imprime etapas e outras infos uteis para o programador ou usuario avancado*/ 
+
+
+/* Definicao de strings, nomes de arquivos, etc */
+#define MODOA "a"
+#define MODOR "r"
+#define MODOW "w"
+
+/* Definicao de tipos */
+typedef struct {
+  int a;
+  int b;
+} tipoParDeInt;
---
+++ trunk/B+/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+/* Interface de utilizacao da arvore B+ */
+int main(){
+
+  
+  
+
+  return(0);
+}
