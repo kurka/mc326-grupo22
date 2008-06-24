58277d0540958652f7b64e6f648c046c3c62421b
felipe.eltermann <felipe.eltermann@8218270c-4048-0410-9d58-cd40a97f162f>
2008-06-24 17:58:10 +0000

    Adicionada interface no main (simples, funcional e suficiente). PS: Mantenham o main limpo, usem outros .c

--- trunk/B+/main.c
+++ trunk/B+/main.c
@@ -51,13 +51,55 @@ insere(tipoNo *arvore){
 int main(){
 
   tipoNo *arvore = (tipoNo *) malloc(sizeof(tipoNo)*100);
+  char opcao, c;
 
   insere(arvore);
+
+  /* Interface*/
+  do {
+    
+    printf("Escolha uma das opcoes abaixo e tecle Enter.\n");
+    printf("1-) Inserir elemento na arvore.\n");
+    printf("2-) Consultar elemento na arvore.\n");
+    printf("3-) Remover elemento da arvore.\n");
+    printf("4-) Sair.\n");
+
+    opcao=getchar();
+    c=getchar();
+    
+    /* Tratamento de caractere invalido ou caractere(s) a mais */
+    if( (c!='\n') || ((opcao!=INSERE)&&(opcao!=CONSULTA)&&(opcao!=REMOVE))){
+      while(c!='\n') c=getchar();
+      printf("Caractere ou sequencia de caracteres invalido.\n\n\n");
+      opcao=0;
+    }
+    
+    switch(opcao){
+
+    case INSERE:
+      /* ... */
+      break;
+
+    case CONSULTA:
+      /* ... */
+      break;
+
+    case REMOVE:
+      /* ... */
+      break;
+
+    }/*fim 'switch'*/
+
+  } while(opcao!=SAIR); /*fim 'do-while'*/
 
   /*
 
   //remove();
   //consulta();  
+  */
+
+  /* 
+  //Lembrar: frees e fcloses...
   */
 
   return(0);
